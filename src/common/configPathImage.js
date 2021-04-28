1cc36f5cf5bd8270a4dea662a17c4d6f31f1f28b
Hung Nguyen <hungnguyen99.nvh@gmail.com>
2021-04-28 15:46:14 +0700

    update host .env

--- src/common/configPathImage.js
+++ src/common/configPathImage.js
@@ -2,11 +2,12 @@ require("dotenv").config();
 const HOST = process.env.SV_HOST;
 const PORT = process.env.SV_PORT;
 const PRODUCT = process.env.PRODUCT;
+const HOST_HEROKU = process.env.HOST_HEROKU;
 
 const configPath = (path) => {
 	// console.log(path);
 	return PRODUCT === "true"
-		? "https://onlinequiz-server.herokuapp.com/" + path
+		? HOST_HEROKU + path
 		: `http://${HOST}:${PORT}/${path}`;
 };
 
