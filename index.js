7aada474c7f2a138f7eb29fdcb4acfc4aeb84c62
Hung Nguyen <hungnguyen99.nvh@gmail.com>
2021-05-08 00:32:58 +0700

    deploy, disable http logger

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const morgan = require("morgan");
+// const morgan = require("morgan");
 require("dotenv").config();
 
 const app = express();
@@ -10,7 +10,7 @@ const { HOST_PORT } = require("./src/constants");
 const routes = require("./src/routes");
 
 // HTTP Logger
-app.use(morgan("dev"));
+// app.use(morgan("dev"));
 
 //Parser Body
 app.use(bodyParser.urlencoded({ extended: false }));
