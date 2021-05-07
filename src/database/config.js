0a44799465b33b2373dd9a6a0f62c0d97856611f
Hung Nguyen <hungnguyen99.nvh@gmail.com>
2021-05-08 00:15:21 +0700

    fix env

--- src/database/config.js
+++ src/database/config.js
@@ -1,10 +1,11 @@
 const mysql = require("mysql2");
 require("dotenv").config();
+const { IS_PRODUCT } = require("./../constants");
 
-const HOST = process.env.DB_HOST;
-const USER = process.env.DB_USER;
-const PASSWORD = process.env.DB_PASS;
-const DBNAME = process.env.DB_NAME;
+const HOST = IS_PRODUCT ? process.env.DB_HOST_PRODUCT : process.env.DB_HOST_DEV;
+const USER = IS_PRODUCT ? process.env.DB_USER_PRODUCT : process.env.DB_USER_DEV;
+const PASSWORD = IS_PRODUCT ? process.env.DB_PASS_PRODUCT : process.env.DB_PASS_DEV;
+const DBNAME = IS_PRODUCT	? process.env.DB_NAME_PRODUCT : process.env.DB_NAME_DEV;
 
 const connectDB = mysql.createConnection({
 	host: HOST,
