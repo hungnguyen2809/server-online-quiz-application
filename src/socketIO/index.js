099952e45ddbd7cda794f055d0e41edf4dff0657
Hung Nguyen <hungnguyen99.nvh@gmail.com>
2021-06-30 00:35:18 +0700

    update socket post_comment

--- src/socketIO/index.js
+++ src/socketIO/index.js
@@ -1,10 +1,16 @@
 const { Server } = require("socket.io");
+const io = new Server({ cors: { origin: "*" } });
 const {
 	SOCKET_CLIENT_SEND_PROFILE,
 	SOCKET_CLIENT_SEND_NEW_POST,
 	SOCKET_SERVER_SEND_NEW_POST,
+	SOCKET_CLIENT_SEND_JOIN_ROOM_POST,
+	SOCKET_CLIENT_SEND_LEAVE_ROOM_POST,
+	SOCKET_CLIENT_SEND_USER_FOCUS_POSTCOMMENT,
+	SOCKET_SERVER_SEND_USER_FOCUS_POSTCOMMENT,
+	SOCKET_CLIENT_SEND_USER_UNFOCUS_POSTCOMMENT,
+	SOCKET_SERVER_SEND_USER_UNFOCUS_POSTCOMMENT,
 } = require("./constant");
-const io = new Server({ cors: { origin: "*" } });
 
 io.on("connection", (socket) => {
 	console.log("Co nguoi ket noi: ", socket.id);
@@ -16,6 +22,38 @@ io.on("connection", (socket) => {
 	//POST
 	socket.on(SOCKET_CLIENT_SEND_NEW_POST, () => {
 		socket.broadcast.emit(SOCKET_SERVER_SEND_NEW_POST);
+	});
+
+	// Join vào room của bài viết
+	socket.on(SOCKET_CLIENT_SEND_JOIN_ROOM_POST, (data) => {
+		const nameRoomPost = `POST_POSTCOMMENT_${data.id_post}`;
+		socket.nameRoomPost = nameRoomPost;
+		socket.join(nameRoomPost);
+		// console.log("JOIN ", io.sockets.adapter.rooms.keys());
+	});
+	// Leave ra khỏi room của bài viết
+	socket.on(SOCKET_CLIENT_SEND_LEAVE_ROOM_POST, (data) => {
+		const nameRoomPost = `POST_POSTCOMMENT_${data.id_post}`;
+		socket.nameRoomPost = "";
+		socket.leave(nameRoomPost);
+		// console.log("LEAVE", io.sockets.adapter.rooms.keys());
+	});
+
+	// Hiển thị có người đang nhập bình luận
+	socket.on(SOCKET_CLIENT_SEND_USER_FOCUS_POSTCOMMENT, () => {
+		if (socket.nameRoomPost) {
+			socket.broadcast
+				.in(socket.nameRoomPost)
+				.emit(SOCKET_SERVER_SEND_USER_FOCUS_POSTCOMMENT);
+		}
+	});
+	//Hủy bỏ hiển thị khi không có người nhập bình luận
+	socket.on(SOCKET_CLIENT_SEND_USER_UNFOCUS_POSTCOMMENT, () => {
+		if (socket.nameRoomPost) {
+			socket.broadcast
+				.in(socket.nameRoomPost)
+				.emit(SOCKET_SERVER_SEND_USER_UNFOCUS_POSTCOMMENT);
+		}
 	});
 });
 
